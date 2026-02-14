657549b2768cbd716d10e3cc9a1de8bcbd6156d3
Mark Lodato <marklodato0@gmail.com>
2026-02-13 21:29:06 -0500

    Read all the data, but don't do anything with it
    At first, we used a buffer size of 4096, because we felt like it. That
    took about 2s to run. We then tried some larger buffer sizes with:
    
    ```sh
    hyperfine -L buf_size 4096,8192,16384,32768,65536 "./target/release/one_brs measurements.txt {buf_size}"
    ```
    
    Of these buffer sizes, 65k was optimal, so we stuck with that. Nearly
    approaching `cat`, we feel pretty good.
    
    Co-authored-by: Andrew Halle <me@andrewhalle.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
-    let path = std::env::args().next_back().expect("Provide a path");
-    let file = std::fs::File::open(path).expect("path should be readable");
-    println!(
-        "{}",
-        file.metadata().expect("metadata should be gettable").len()
-    );
+    let path = std::env::args().nth(1).expect("Provide a path");
+    let mut file = std::fs::File::open(path).expect("path should be readable");
+    let mut buf = vec![0; 65_536];
+
+    let mut len = 0;
+    loop {
+        match std::io::Read::read(&mut file, &mut buf) {
+            Ok(0) => break,
+            Ok(n) => len += n,
+            Err(e) if e.kind() == std::io::ErrorKind::Interrupted => {}
+            Err(e) => panic!("Failed read: {e:?}"),
+        }
+    }
+    println!("{}", len);
 }
